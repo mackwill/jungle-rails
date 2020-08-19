e00a8e4d50535bcc0d8de1ad59f3856e81f33f73
William <mack.mindenhall@gmail.com>
2020-08-19 11:21:27 +0000

    Added list to show total number of products and categories on  admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,10 @@
 class Admin::DashboardController < ApplicationController
 
   http_basic_authenticate_with name: ENV['ADMIN_EXAMPLE_USER'], password: ENV['ADMIN_EXAMPLE_PASSWORD']
-  # http_basic_authenticate_with name: "Jungle", password: "book"
+
+
   def show
+    @products = Product.all
+    @categories = Category.all
   end
 end
